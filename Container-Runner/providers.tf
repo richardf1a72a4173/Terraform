5836a15853bb3588dabbdfa7d531103300e63d70
Richard F <richardmfarmer@gmail.com>
2025-06-22 15:52:37 +0100

    Update providers.tf

--- Container-Runner/providers.tf
+++ Container-Runner/providers.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   features {}
 
-  client_id = "00000000-0000-0000-0000-000000000000" # spn-c-t0.richard.-var
+  client_id = "00000000-0000-0000-0000-000000000000"
   # client_secret = var.client_secret
   client_certificate_path     = var.client_certificate_path
   client_certificate_password = var.client_certificate_password
@@ -21,7 +21,7 @@ provider "azurerm" {
   subscription_id = "00000000-0000-0000-0000-000000000000" # Platform
   features {}
 
-  client_id = "00000000-0000-0000-0000-000000000000" # spn-c-t0.richard.-var
+  client_id = "00000000-0000-0000-0000-000000000000"
   # client_secret = var.client_secret
   client_certificate_path     = var.client_certificate_path
   client_certificate_password = var.client_certificate_password
